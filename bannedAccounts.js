5b6f722fb3e8764bbfb6b413c8a2af13daf26aef
yummyweb <antriksh.timepass@gmail.com>
2023-07-13 16:04:07 +0530

    more banned

--- bannedAccounts.js
+++ bannedAccounts.js
@@ -2,7 +2,9 @@
 const bannedAccounts = [
     "A9W7DD3A9P2WUeTNZBn5LMwo5zcNq2fmxWW9e6R9ro9c",
     "2i28DPP9sMDRB48xnW5jSkM913peiYbLCdS7uzqeGRf3",
-    "ELhK9qiVtBkSUgihmsB9YTvd639MZ8gD89XzT4JyWLVF"
+    "ELhK9qiVtBkSUgihmsB9YTvd639MZ8gD89XzT4JyWLVF",
+    "FV1oJSuAx5dZyr8kFqbr1AGJKLg7YEcGB6oeNeL4znis",
+    "Cps1XennyUXogHev6SAZV2XiUdMTXvQChUFaJkr99EHS"
 ]
 
 return bannedAccounts
